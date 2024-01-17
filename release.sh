9a6c15abc53aa0ad24b67ed2f114a8a9e1760611
Robbie <robbie.huffman@nundrum.net>
2024-01-16 21:42:34 -0500

    changed release filename to meet github requirements

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 
 mkdir -p target
-tar vzcf target/N3WT48.tgz --transform s@resources/public/@@ resources/public/index.html resources/public/main.css resources/public/pages.js resources/public/img resources/public/js/compiled/app.js 
+tar vzcf target/N3WT48.tar.gz --transform s@resources/public/@@ resources/public/index.html resources/public/main.css resources/public/pages.js resources/public/img resources/public/js/compiled/app.js 
 
