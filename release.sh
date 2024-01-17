b49a036d899f070235e81a4b90c9f30de3238e95
Robbie <robbie.huffman@nundrum.net>
2024-01-16 21:28:56 -0500

    added release.sh to build release tgz

---
+++ release.sh
@@ -0,0 +1,4 @@
+
+mkdir -p target
+tar vzcf target/N3WT48.tgz --transform s@resources/public/@@ resources/public/index.html resources/public/main.css resources/public/pages.js resources/public/img resources/public/js/compiled/app.js 
+
